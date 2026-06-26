8d4496b58cda739b75759cea7ab2920a5fe1dda6
Haniful Islam <hanifulislam160@gmail.com>
2026-06-26 23:30:30 +0600

    registerUser function is show separate component

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,27 +1,21 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import httpStatus from "http-status";
 import { userServices } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
 
-const registerUser = async (req: Request, res: Response) => {
-  try {
-    const payload = req.body;
+const registerUser = catchAsync(async (req: Request, res: Response) => {
+  const payload = req.body;
 
-    const user = await userServices.registerIntoDB(payload);
+  const user = await userServices.registerIntoDB(payload);
 
-    res.status(httpStatus.CREATED).json({
-      success: true,
-      message: "User created successfully",
-      statusCode: httpStatus.CREATED,
-      data: { user },
-    });
-  } catch (error:any) {
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      message:"Failed to register User",
-      error: error.message
-    });
-  }
-};
+  res.status(httpStatus.CREATED).json({
+    success: true,
+    message: "User created successfully",
+    statusCode: httpStatus.CREATED,
+    data: { user },
+  });
+});
+
 
 export const userController = {
   registerUser,
