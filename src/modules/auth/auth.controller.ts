503a2944fe90aba5544c8b17ac1d536bf6c6596c
Haniful Islam <hanifulislam160@gmail.com>
2026-06-27 03:03:53 +0600

    cookie set

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -8,13 +8,28 @@ const loginUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
 
-    const user = await authServices.loginUser(payload);
+    const {accessToken, refreshToken} = await authServices.loginUser(payload);
+
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24, 
+
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
 
     sendResponse(res, {
       success: true,
       statusCode: httpStatus.OK,
       message: "User logged in successfully",
-      data: user,
+      data: { accessToken, refreshToken },
     });
   },
 );
