66e774b73c701ff43b365c5c3dedf3ae3b48bd3a
Haniful Islam <hanifulislam160@gmail.com>
2026-06-26 23:55:19 +0600

    response reusable

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,19 +1,23 @@
+
 import { NextFunction, Request, RequestHandler, Response } from "express";
 import httpStatus from "http-status";
 import { userServices } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+
 
 const registerUser = catchAsync(async (req: Request, res: Response) => {
   const payload = req.body;
 
   const user = await userServices.registerIntoDB(payload);
 
-  res.status(httpStatus.CREATED).json({
+  sendResponse(res, {
     success: true,
-    message: "User created successfully",
     statusCode: httpStatus.CREATED,
-    data: { user },
-  });
+    message: "User created successfully",
+    data: user,
+  })
+  
 });
 
 
